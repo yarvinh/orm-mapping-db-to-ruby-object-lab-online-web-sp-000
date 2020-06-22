330566bf3a564f5117b37524f4356996dcd39bf0
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-22 21:23:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
 
   def self.new_from_db(row)
       student = self.new
-      students.id = row[0]
+      student.id = row[0]
       student.name =  row[1]
       student.grade = row[2]
       student
