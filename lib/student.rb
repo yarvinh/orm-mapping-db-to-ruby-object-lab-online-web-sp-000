331c8ed0b9aa6aabb3996bdafc224f6b7e780ef2
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2020-06-22 21:57:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,6 @@ class Student
     SELECT * FROM students
     WHERE grade < 12
     SQL
-     p self.new_from_db(DB[:conn].execute(sql))
+     p self.new_from_db(DB[:conn].execute(sql)[0])
   end
 end
